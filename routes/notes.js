f9536b0ac1757f6ecb54d2755579eb6afa94d3fb
Yuki <morechan216@gmail.com>
2025-06-05 17:47:51 +0900

    Add mongodb access

--- routes/notes.js
+++ routes/notes.js
@@ -1,19 +1,23 @@
-var express= require('express');
-var router= express.Router();
+var express = require('express');
+var router = express.Router();
 
 
 
 // 接続情報を設定
-const{ MongoClient} = require("mongodb");
-const uri= "**********";
-const client= new MongoClient(uri);
-router.get('/', async(req, res) =>{
+const { MongoClient } = require("mongodb");
+const uri = "**********";
+const client = new MongoClient(uri);
+
+router.get('/', async (req, res) => {
 // データベース、コレクションを指定
-const database= client.db('notes');
-const notes= database.collection('notes');
+const database = client.db('notes');
+const notes = database.collection('notes');
+
 // idが１のドキュメントを取得
-const query= { id:2};
-const note= await notes.findOne(query);
+const query = { id: 2 };
+const note = await notes.findOne(query);
+
 res.json(note);
 })
-module.exports= router;
+
+module.exports = router;
